03070015b6db980051fbc483b859ad33dc771c2c
lIlKyMalIl <kyma9965@gmail.com>
2016-04-21 08:55:49 +0300

    Update assignment8.ino

--- assignment8.ino
+++ assignment8.ino
@@ -1,3 +1,4 @@
+
 #include <Smartcar.h>
 
 Odometer encoderLeft(190), encoderRight(190); //Βάλτε τους δικούς σας παλμούς ανά μέτρο
@@ -5,8 +6,8 @@ Gyroscope gyro(13); //Βάλτε την κατάλληλη τιμή σύμφων
 Car folkracer;
 
 SR04 front;
-const int TRIGGER_PIN = 6; //Βάλτε την κατάλληλη τιμή για το pin που είναι συνδεδεμένο στο trigger
-const int ECHO_PIN = 7; //Βάλτε την κατάλληλη τιμή για το pin που είναι συνδεδεμένο στο echo
+const int TRIGGER_PIN = 3; //Βάλτε την κατάλληλη τιμή για το pin που είναι συνδεδεμένο στο trigger
+const int ECHO_PIN = 2; //Βάλτε την κατάλληλη τιμή για το pin που είναι συνδεδεμένο στο echo
 
 void setup() {
   front.attach(TRIGGER_PIN, ECHO_PIN); //αρχικοποιεί τον υπέρηχο
@@ -26,8 +27,9 @@ void loop() {
   int distance = front.getDistance(); //μέτρησε την απόσταση και αποθήκευσέ την στη μεταβλητή distance
 
   //TO-DO: Κάντε το αυτοκινητάκι να στρίβει 90 μοίρες προς τα αριστερά όταν βρίσκει ένα κοντινό εμπόδιο και να συνεχίζει την πορεία του
-  if (distance < 20 && distance > 0){
+  if (distance < 30 && distance > 0){
+    folkracer.stop();
     folkracer.rotate(-90);
+    folkracer.setSpeed(0.5);
   }
 }
-
